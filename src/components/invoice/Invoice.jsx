b77db0a1cef389a61c21477eb3e1b6c4aca7b297
Edwin Cruz <edwin.cruz13@hotmail.com>
2023-01-26 01:19:58 -0600

    adding styles lines

--- src/components/invoice/Invoice.jsx
+++ src/components/invoice/Invoice.jsx
@@ -18,7 +18,43 @@ export const Invoice = () => {
 
                     <div className="btn-group">
                         <button className="btn btn-default rounded-left">-</button>
-                        <button className="btn">1</button>
+                        <span>5</span>
+                        <button className="btn btn-default rounded-right">+</button>
+                    </div>
+                </div>
+            </div>
+
+            <div data-id="1" className="card">
+                <div className="image">
+                    <img src={burger1} alt="" />
+                </div>
+
+                <div className="details">
+                
+                    <h1>Burger chess and jam</h1>
+                    <h2 className="item-price">$25.4</h2>
+
+                    <div className="btn-group">
+                        <button className="btn btn-default rounded-left">-</button>
+                        <span>5</span>
+                        <button className="btn btn-default rounded-right">+</button>
+                    </div>
+                </div>
+            </div>
+
+            <div data-id="1" className="card">
+                <div className="image">
+                    <img src={burger1} alt="" />
+                </div>
+
+                <div className="details">
+                
+                    <h1>Burger chess and jam</h1>
+                    <h2 className="item-price">$25.4</h2>
+
+                    <div className="btn-group">
+                        <button className="btn btn-default rounded-left">-</button>
+                        <span>5</span>
                         <button className="btn btn-default rounded-right">+</button>
                     </div>
                 </div>
