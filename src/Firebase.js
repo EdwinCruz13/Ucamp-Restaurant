a1f5590a08eeccb1b1612a029034046b053dbf65
edwin.cruz <edwin.cruz@EDWIC>
2023-01-27 16:56:03 -0600

    adding firebase scripts

---
+++ src/Firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBkmrHn-N2J5NJzeUuh1thipx9jjS-jNo4",
+  authDomain: "historyapp-f4db2.firebaseapp.com",
+  databaseURL: "https://historyapp-f4db2.firebaseio.com",
+  projectId: "historyapp-f4db2",
+  storageBucket: "historyapp-f4db2.appspot.com",
+  messagingSenderId: "96070114588",
+  appId: "1:96070114588:web:0ce407e0431aaa82442ed9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
